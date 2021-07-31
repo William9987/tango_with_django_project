1e2865632f5b57c553631f6bb9a779734c39e6a7
William Lie <2512239l@student.gla.ac.uk>
2021-07-31 20:37:59 +0700

    Chapter 6 commit

--- rango/admin.py
+++ rango/admin.py
@@ -3,3 +3,8 @@ from rango.models import Category, Page
 
 admin.site.register(Category)
 admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
